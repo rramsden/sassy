32c4a7b20f68be673c32d8697f48e9bacc2e5d7e
Richard Ramsden <richard@rramsden.ca>
2012-11-02 23:20:56 -0700

    Add tests!

---
+++ test/sassy_test.erl
@@ -0,0 +1,18 @@
+-module(sassy_test).
+-include_lib("eunit/include/eunit.hrl").
+
+outputs_error_message_test() ->
+    {error, Msg} = sassy:compile("broken"),
+    ?assert(is_list(Msg)).
+
+outputs_compiled_sass_test() ->
+    {ok, Compiled} = sassy:compile("$color: red; #test{ color: $color; }"),
+    ?assertEqual(Compiled, "#test {\n  color: red; }\n" ).
+
+compiles_from_file_test() ->
+    Path = code:priv_dir(sassy),
+    Ex1 = Path ++ "/test/example.scss",
+    ?assertMatch({ok, _}, sassy:compile_file(Ex1)),
+
+    Ex2 = Path ++ "/test/example.sass",
+    ?assertMatch({ok, _}, sassy:compile_file(Ex2)).
